0822f45cdb6a1e2cd8bae93bf402ac72f1414fbe
Lee McAlilly <leemcalilly@gmail.com>
2011-04-05 17:44:18 -0400

    maybe this will work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :check_uri
 
-
   def check_uri
-    if /^www/.match(request.host)
-      redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri 
-    end
+      redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host)
   end
-
-    
 end
