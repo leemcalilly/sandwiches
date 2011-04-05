37973cad88dea2a0e775bd6930ef1fce4c9ad2ae
Lee McAlilly <leemcalilly@gmail.com>
2011-04-05 17:41:10 -0400

    trying something else to redirect www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+  before_filter :check_uri
   before_filter :ensure_domain
-  before_filter :add_www_subdomain
 
   APP_DOMAIN = 'countryclubsandwiches.com'
 
+  def check_uri
+    if /^www/.match(request.host)
+      redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri 
+    end
+  end
+  
   def ensure_domain
     if request.env['HTTP_HOST'] != APP_DOMAIN
       # HTTP 301 is a "permanent" redirect
@@ -14,10 +19,5 @@ class ApplicationController < ActionController::Base
   end
   
   private
-    def add_www_subdomain
-      unless /^www/.match(request.host)
-        redirect_to("#{request.protocol}x.com#{request.request_uri}",
-                    :status => 301)
-      end
-    end
+    
 end
